ddf5d1dd90c815f3bdf5907475ba62568e9d34bc
Madson <madsonsmitt@gmail.com>
2025-12-14 16:11:09 -0300

    Modelagem do esquema de pastas e da arquitetura feita
    Adiciona `Jest` como TestRunner do projeto
    
    Implementação de uma classe de teste de exemplo
    
    Closes #6

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-  return <h1>New deployment commit test for Home Page.</h1>
+  return <h1>New deployment commit test for Home Page.</h1>;
 }
 
-export default Home
+function Teste() {
+  console.log('teste');
+}
+
+export default Home;
