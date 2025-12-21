d58e567cdd25a2061e8bd67b2cfc2132181cc190
Madson <madsonsmitt@gmail.com>
2025-12-21 13:56:19 -0300

    add `.env` config file and abstraction around it
    add `pg` module config

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,9 @@
-function status(req, res) {
+import database from '../../../../infra/database.js';
+
+async function status(req, res) {
+  const result = await database.query('SELECT 1+1 as sum;');
+  console.log(result.rows);
+
   res.setHeader('x-creator', 'é o demonzin');
 
   res.status(200).json({
